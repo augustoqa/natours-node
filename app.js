d16a2c7d2a93609e7bc4dfdb079a2055fba7f71a
Cesar Acual <augustoacual@gmail.com>
2021-08-05 12:24:40 -0600

    Create our own middleware

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) Route Handlers
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     retults: tours.length,
     data: {
       tours,
@@ -86,6 +101,7 @@ const deleteTour = (req, res) => {
   });
 };
 
+// 3) Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -94,6 +110,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+// Start the Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
