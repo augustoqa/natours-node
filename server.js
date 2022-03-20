3cd9b41a9ecdcd8a2955dea8adc4bf8f09100aa4
César Acual <augustoacual@gmail.com>
2022-03-19 23:18:47 -0600

    Create a Simple Tour Model

--- server.js
+++ server.js
@@ -18,6 +18,24 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour mus have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tourmus have a price'],
+  },
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
