69e7f78510a90699a521340d2288da6133fd29db
César Acual <augustoacual@gmail.com>
2022-03-24 17:42:06 -0600

    Reading Document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,26 +1,41 @@
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    // retults: tours.length,
-    // data: {
-    //   tours,
-    // },
-  })
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find()
+
+    res.status(200).json({
+      status: 'success',
+      retults: tours.length,
+      data: {
+        tours,
+      },
+    })
+  } catch (err) {
+    res.send(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
 }
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1
-  // const tour = tours.find((tour) => tour.id === id)
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id)
+    // Tour.findOne({ _id: req.params.id })
 
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    })
+  } catch (err) {
+    res.send(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
 }
 
 exports.createTour = async (req, res) => {
