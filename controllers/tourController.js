68413e3328f243cf48945c59245afc25d43139fd
César Acual <augustoacual@gmail.com>
2022-05-09 19:07:05 -0600

    Making the API Better Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v')
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+
+    // page=3&limit=10, 1-10, page 1, 11-20, page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit)
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()
+      if (skip >= numTours) throw new Error('This page does not exist')
+    }
+
     // EXECUTE QUERY
     const tours = await query
 
@@ -43,7 +56,7 @@ exports.getAllTours = async (req, res) => {
       },
     })
   } catch (err) {
-    res.send(404).json({
+    res.status(404).json({
       status: 'fail',
       message: err,
     })
